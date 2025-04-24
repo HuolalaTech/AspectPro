a9a25a42f41cb08993582ff2038f3ef66b1da948
greek.chen <greek.chen@huolala.cn>
2025-04-24 10:38:47 +0800

    9.delete signingConfigs

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,23 +1,15 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 import * as path from 'path';
-// 依赖远程aop-pro-plugin
-// import { aspectProPlugin } from 'aspect-pro-plugin';
-//
-// export default {
-//   system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-//   plugins: [aspectProPlugin()]
-// }
 
 // TODO 开发阶段 - 依赖local plugin
-// import { aspectProPluginV2 } from '../local_plugin/src/main/AspectProPluginV2';
-//
-// export default {
-//   system: hapTasks,
-//   plugins: [aspectProPluginV2(/*'../local_plugin/src/main/compile/AspectCompilePlugin.ts'*/)]
-// }
+/*import { aspectProPluginV2 } from '../local_plugin/src/main/AspectProPluginV2';
+
+export default {
+  system: hapTasks,
+  plugins: [aspectProPluginV2('../local_plugin/src/main/compile/AspectCompilePlugin.ts')]
+}*/
 
 // 依赖远程aop-pro-plugin v2
-// import { aspectProPluginV2 } from '/Users/greek.chen/HarmonyOs/openSource/AspectPro/aop_plugin/src/main/AspectProPluginV2.ts';
 import { aspectProPluginV2} from 'aspect-pro-plugin';
 export default {
   system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
